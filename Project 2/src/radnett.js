fb5b5f2cf1e70f6aa41a38737df417111e4a9e73
Andrea Giorgi <andrea_giorgi@outlook.it>
2021-07-22 20:10:36 +0200

    [Map] DATA BITCH [CSS] New colors [Script] Data handling introduced

--- Project 2/src/radnett.js
+++ Project 2/src/radnett.js
@@ -1,18 +1,12 @@
 
 let norwayData //Storage for topology data
 let rawNorwayData //Storage for raw geojson data
-let radnettData //Storage for radiation data
 let canvas = d3.select('#canvas') //D3 selection
 let projection
 let path
 
-const color_domain = [0.080, 0.090, 0.095, 0.100, 0.110, 0.115];
-const color_legend = d3.scaleThreshold().range(['#fee5d9', '#fcbba1', '#fc9272',  '#fb6a4a', '#de2d26', '#a50f15']).domain(color_domain);
-
-let colorFunction = () => {
-	return "white";
-}
-
+const color_domain = [0.080, 0.090, 0.100, 0.110, 0.120, 0.125, 0.130];
+const color_legend = d3.scaleThreshold().range(['#ccdcd9', '#80a9a0', '#67978d', '#4d867a',  '#347566', '#1b6454', '#025341']).domain(color_domain);
 
 let drawMap = () => {
    
@@ -23,32 +17,30 @@ let drawMap = () => {
 		.attr('d', path)
 		.attr('stroke', "#F5FBEF")
 		.attr('stroke-width', 0.8)
-		.attr('fill', "#de2d26")
+		.attr('fill', (d) => {
+			const value = d['properties']['average_radiation_value'];
+			if (value) {
+			  return color_legend(d['properties']['average_radiation_value']);
+			} else {
+				return '#ccc';}
+			})
 		.classed('svg-content-responsive', true);
 }
 
-d3.json("map_data_topo.json").then(
+d3.json("map_data_topo_1617-07.json").then(
 	(data, error) => {
 		if (error) {
 			console.log(error);
 		}else{
-			rawNorwayData = topojson.feature(data, data.objects.collection);
+			rawNorwayData = topojson.feature(data, data.objects.map_data_topo);
 			projection = d3.geoMercator().fitSize([970, 620], rawNorwayData);
 			path = d3.geoPath().projection(projection);
 			
-			norwayData = topojson.feature(data, data.objects.collection).features;
+			norwayData = topojson.feature(data, data.objects.map_data_topo).features;
 			console.log(norwayData);
-			d3.json("data\/radnett_data_1617-07-2021.json").then(
-				(data, error) => {
-					if(error){
-						console.log(error);
-					}else{
-						radnettData = data;
-						console.log(radnettData);
-						drawMap();
-					}
-				}
-			)
-		}
 
-})
+			drawMap();
+		}
+	}
+
+)
